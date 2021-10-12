a57499679d724105b7c0f015f566bfe0b12f957c
javierg <javierg@amcoonline.net>
2021-10-12 15:15:03 -0700

    push patch version 0.1.6

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Couchx.MixProject do
   def project do
     [
       app: :couchx,
-      version: "0.1.5",
+      version: "0.1.6",
       elixir: "~> 1.12",
       name: "Couchx",
       description: "Limited CouchDb Adapter for Ecto",
